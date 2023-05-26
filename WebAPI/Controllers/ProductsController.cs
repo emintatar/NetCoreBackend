10981629ca917d531ed4636ad2095273c0f07318
Emin Tatar <emintatar48@outlook.com>
2023-05-26 15:24:16 +0300

    API layer added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;       
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var dataResult = _productService.GetAll();
+            if (dataResult.Success)
+            {
+                return Ok(dataResult);
+            }
+
+            return BadRequest(dataResult);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var dataResult = _productService.GetById(id);
+            if (dataResult.Success)
+            {
+                return Ok(dataResult);
+            }
+
+            return BadRequest(dataResult);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product) 
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
